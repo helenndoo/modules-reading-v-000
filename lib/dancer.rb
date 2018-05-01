a04640efe10ea3dc5f8c9ff106131e8bc5414ff8
Helen Du <helendu.614@gmail.com>
2018-05-01 15:14:37 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,10 +1,10 @@
-require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
 require_relative './dance_module.rb'
 
 class Dancer
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
-  
+
 
   attr_accessor :name
 
@@ -12,4 +12,3 @@ class Dancer
     @name = name
   end
 end
-
