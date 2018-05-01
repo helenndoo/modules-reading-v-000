5dfe52de7d144edfd7104f9b36894d1a6b707ad0
Helen Du <helendu.614@gmail.com>
2018-05-01 15:22:31 +0000

    automatically backed up by learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -12,4 +12,5 @@ module Dance
   end
 
   def take_a_bow
-    "Thank you, thank you. It was a pleasure to dance for you at all."
+    "Thank you, thank you. It was a pleasure to dance for you all."
+  end
